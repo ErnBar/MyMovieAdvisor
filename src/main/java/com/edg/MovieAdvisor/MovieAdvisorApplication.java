b0907b1bddec642e270abc3d0b883b529afeaf05
DavrK <dl.davrk@gmail.com>
2024-06-05 18:28:48 +0200

    Added Spring Initializr to the project

---
+++ src/main/java/com/edg/MovieAdvisor/MovieAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.edg.MovieAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edg/MovieAdvisor/MovieAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edg.MovieAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
