e263d610f2f6fad21238619ac8f4bcde706e0b78
DavrK <dl.davrk@gmail.com>
2024-06-05 19:17:30 +0200

    Added Worker (Director/Actor) and Movie models

---
+++ src/main/java/com/edg/MovieAdvisor/models/Movie.java
@@ -0,0 +1,38 @@
+package com.edg.MovieAdvisor.models;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.SQLJoinTableRestriction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name="director_id")
+    @SQLJoinTableRestriction("role = 'DIRECTOR'")
+    private Worker director;
+}
---
+++ src/main/java/com/edg/MovieAdvisor/models/Worker.java
@@ -0,0 +1,38 @@
+package com.edg.MovieAdvisor.models;
+
+import java.sql.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "workers")
+public class Worker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Date birthday;
+
+    @Column(nullable = false)
+    private String nationality;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "director")
+    private List<Movie> movies;
+}
