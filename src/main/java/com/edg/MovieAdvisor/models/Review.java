f42d200066e9527138a3789d8a998dccf88e30bc
ErnBar <ernest23.barri@gmail.com>
2024-06-05 19:43:29 +0200

    Update Review.java

--- src/main/java/com/edg/MovieAdvisor/models/Review.java
+++ src/main/java/com/edg/MovieAdvisor/models/Review.java
@@ -1,10 +1,12 @@
 package com.edg.MovieAdvisor.models;
 
-import java.sql.Date;
+
+
+import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
