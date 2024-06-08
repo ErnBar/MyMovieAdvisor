d9784a769051800995f684b9477ab818592350b6
ErnBar <ernest23.barri@gmail.com>
2024-06-08 20:02:42 +0200

    Base64profilepicture method
    Aggiunta di colonna base64 al db tramite user, definizione del metodo che converte in base 64 il jpeg mandato in input e aggiornamento html per la lettura del base64

--- src/main/java/com/edg/MovieAdvisor/MovieAdvisorApplication.java
+++ src/main/java/com/edg/MovieAdvisor/MovieAdvisorApplication.java
@@ -2,8 +2,10 @@ package com.edg.MovieAdvisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.edg.MovieAdvisor.repositories")
 public class MovieAdvisorApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/edg/MovieAdvisor/models/User.java
+++ src/main/java/com/edg/MovieAdvisor/models/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -32,6 +33,10 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String profilepicturebase64;
 
     @OneToMany(mappedBy = "op", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews;
