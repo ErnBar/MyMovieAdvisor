6b018b908b41bc91889c08b7aa023fbdf4782d33
ErnBar <ernest23.barri@gmail.com>
2024-06-13 00:51:42 +0200

    Test add NativeQuery.
    Da provare tra qualche giorno quando le review saranno definite

--- src/main/java/com/edg/MovieAdvisor/repositories/MovieRepository.java
+++ src/main/java/com/edg/MovieAdvisor/repositories/MovieRepository.java
@@ -1,9 +1,15 @@
 package com.edg.MovieAdvisor.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.edg.MovieAdvisor.models.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     Movie findByTitle(String title);
+    
+    @Query(value = "SELECT m.title FROM movies m LEFT JOIN reviews r ON m.id = r.movie_id GROUP BY m.id ORDER BY AVG(r.score) DESC", nativeQuery = true)
+    List<Movie> findAllMoviesOrderByAverageScoreDesc();
 }
