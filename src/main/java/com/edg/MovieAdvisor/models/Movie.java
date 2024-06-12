0bbd9f11db7723e3567bdf0fd881a98952325c61
ErnBar <ernest23.barri@gmail.com>
2024-06-12 17:28:18 +0200

    fix

--- src/main/java/com/edg/MovieAdvisor/models/Movie.java
+++ src/main/java/com/edg/MovieAdvisor/models/Movie.java
@@ -29,7 +29,7 @@ public class Movie {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false,columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
