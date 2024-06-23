74e1b31b82c9f1726a5e01aadfd75d9ad5fd5f99
ErnBar <ernest23.barri@gmail.com>
2024-06-23 17:12:10 +0200

    useless import remove

--- src/main/java/com/edg/MovieAdvisor/controllers/MovieController.java
+++ src/main/java/com/edg/MovieAdvisor/controllers/MovieController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.edg.MovieAdvisor.models.Actor;
 import com.edg.MovieAdvisor.models.Director;
