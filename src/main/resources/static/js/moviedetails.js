b36b6cc7d4c35eeebe4ea7491e730740fdfdff5d
Alessandro <alearry@gmail.com>
2024-06-24 20:20:50 +0200

    Fine2

--- src/main/resources/static/js/moviedetails.js
+++ src/main/resources/static/js/moviedetails.js
@@ -53,3 +53,30 @@ function searchActors() {
         dropdown.style.display = 'none';
     }
 }
+
+function searchFilm() {
+    var title = document.getElementById("search-film").value;
+    if (title.length > 0) {
+        fetch(`/searchMovies?prefix=${title}`)
+            .then(response => response.json())
+            .then(data => {
+                var results = document.getElementById("search-results");
+                results.innerHTML = ''; // Clear previous results
+                if (data.length > 0) {
+                    data.forEach(movie => {
+                        var li = document.createElement("li");
+                        li.innerText = movie.title;
+                        li.onclick = function() {
+                            window.location.href = `/movieDetail?title=${movie.title}`;
+                        };
+                        results.appendChild(li);
+                    });
+                } else {
+                    results.innerHTML = '<li>No results found</li>';
+                }
+            });
+    } else {
+        document.getElementById("search-results").innerHTML = '';
+    }
+}
+
